d0c7aa0d097d4155e1382591844318be6d85303e
Wu Yu <wuyu@Wus-iMac.local>
2016-10-25 23:24:57 +0800

    add first deriv

--- masolver.py
+++ masolver.py
@@ -31,20 +31,34 @@ class MASolver:
 	def Dxx(self):
 		shape = self.u.shape
 		res = np.zeros(shape)
-		res[:,1:shape[1]-2] = (self.u[:,0:shape[1] - 3] + self.u[:,2:shape[1]-1] - 2.0 * self.u[:,1:shape[1]-2]) / self.grid.dx
+		res[:,1:shape[1]-2] = (self.u[:,0:shape[1] - 3] + self.u[:,2:shape[1]-1] - 2.0 * self.u[:,1:shape[1]-2]) / self.grid.dx /self.grid.dx
 		return res	
 
 	def Dyy(self):
 		shape = self.u.shape
 		res = np.zeros(shape)
-		res[1:shape[0]-2,:] = (self.u[0:shape[0] - 3,:] + self.u[2:shape[0]-1,:] - 2.0 * self.u[1:shape[0]-2,:]) / self.grid.dy
+		res[1:shape[0]-2,:] = (self.u[0:shape[0] - 3,:] + self.u[2:shape[0]-1,:] - 2.0 * self.u[1:shape[0]-2,:]) / self.grid.dy / self.grid.dy
 		return res	
 
 	def Dxy(self):
 		shape = self.u.shape
 		res = np.zeros(shape)
-		res[1:shape[0]-2,:] = (self.u[0:shape[0] - 3,:] + self.u[2:shape[0]-1,:] - 2.0 * self.u[1:shape[0]-2,:]) / self.grid.dy
+		res[1:shape[0]-2, 1:shape[1]-2] = (self.u[0:shape[0] - 3, 0:shape[1]-3] - self.u[0:shape[0] - 3, 2:shape[1]-1]  + 
+			                               self.u[2:shape[0] - 1, 2:shape[1]-1] - self.u[2:shape[0] - 1, 0:shape[1]-3] ) / self.grid.dy / self.grid.dx / 4.0
+		return res
+
+	def Dx(self):
+		shape = self.u.shape
+		res = np.zeros(shape)
+		res[:, 1:shape[1]-2] = (self.u[:, 2:shape[1] - 1] - self.u[:, 0:shape[1] - 3]) / self.grid.dx / 2.0
+		return res				
+	
+	def Dy(self):
+		shape = self.u.shape
+		res = np.zeros(shape)
+		res[1:shape[0]-2, :] = (self.u[2:shape[0] - 1, :] - self.u[0:shape[0] - 3, :]) / self.grid.dy / 2.0
 		return res		
-			
+
 	def solve(self):
-		print self.Dxx()
+		print self.Dxx().shape
+		print self.Dxy().shape
